165ac29e815fe9872b416f2ada58b55bd94933ad
Sainath Wankhede <wankhedesainath1431@gmail.com>
2023-12-24 12:41:11 +0530

    first

---
+++ JavaScript-1/Assignment/operators.js
@@ -0,0 +1,25 @@
+// Write a program that prints the multiplication table in the texbook format of any number specified.
+
+let number = 6;
+
+console.log(` ${number} x 1 = ${number * 1}`);
+console.log(` ${number} x 2 = ${number * 2}`);
+console.log(` ${number} x 3 = ${number * 3}`);
+console.log(` ${number} x 4 = ${number * 4}`);
+console.log(` ${number} x 5 = ${number * 5}`);
+console.log(` ${number} x 6 = ${number * 6}`);
+console.log(` ${number} x 7 = ${number * 7}`);
+console.log(` ${number} x 8 = ${number * 8}`);
+console.log(` ${number} x 9 = ${number * 9}`);
+console.log(` ${number} x 10 = ${number * 10}`);
+
+// 6 x 1 = 6
+// 6 x 2 = 12
+// 6 x 3 = 18
+// 6 x 4 = 24
+// 6 x 5 = 30
+// 6 x 6 = 36
+// 6 x 7 = 42
+// 6 x 8 = 48
+// 6 x 9 = 54
+// 6 x 10 = 60
---
+++ JavaScript-1/Conditions/script.js
@@ -0,0 +1,84 @@
+// conditions.
+
+// Else-if statement
+
+// let age = 17;
+// if (age >= 18) {
+//     console.log("You are allowed!");
+// }
+// else {
+//     console.log("Your are not allowed");
+// }
+
+//Else-if Ladder
+
+// let signal = 'pink';
+// if (signal == 'Red') {
+//     console.log("Red ==> STOP");
+// }
+// else if (signal == 'yellow') {
+//     console.log("Yellow ==> Go Slow");
+// }
+// else if (signal == 'green') {
+//     console.log("Green ==> Go Fast.");
+// }
+// else {
+//     console.log("Invalid");
+// }
+
+// Switch Statement.
+
+// let user = 'sainath';
+// switch (user) {
+//     case 'sainath':
+//         console.log("My name is sainath");
+//         break;
+
+//     case 'shubham':
+//         console.log("My name is shubham");
+//         break;
+
+//     case 'aman':
+//         console.log("My name is Aman.");
+//         break;
+
+//     default:
+//         console.log("Invalid User Name")
+//         break;
+// }
+
+//LOOPS fOR,while , do-while
+
+// for loop
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+//while(init , condition , inc. or dec.)
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// do-while loop //body of do-while loop will be executed at least one time.
+
+// i = 0;
+// do {
+//     console.log("hello world");
+//     i++;
+
+// } while (i < 5);
+
+
+// ternary operator/condition : (condition) true statement : false statement.
+
+// let age = 34;
+// result = (age >= 18) ? "drink wine." : "drink water";
+// console.log(result);
+
+// IsloggedIn = false;
+// IsloggedIn ? console.log("home page") : console.log("log in page");
+
+// javascript is lossely typed programming language because they properly executes its code without declaring the let keyword for exmaple above code is 
